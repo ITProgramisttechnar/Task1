5146d72a88a4c2c9e475f595afb085dcbd900b17
ITProgramisttechnar <samyro001@mail.ru>
2025-12-29 17:48:07 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Tom","Grey", (byte) 12);
+        userService.saveUser("Bill","Brown", (byte) 30);
+        userService.saveUser("Zak","Blue", (byte) 16);
+        userService.saveUser("Joe","Black", (byte) 26);
+        userService.getAllUsers();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
